b517fa57157d4f522255b1357f97be0e319082b2
samkcrespo <samkcrespo@gmail.com>
2022-08-17 17:13:44 -0700

    Add project files.

---
+++ PingApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.NetworkInformation;
+using System.Text;
+
+//Pinging Google DNS server 4.2.2.2
+
+Ping pingSender = new Ping();
+
+PingOptions options = new PingOptions();
+
+options.DontFragment = true;
+
+string data = "Learn to code";
+
+//Needs to be buffered
+byte[] buffer = Encoding.ASCII.GetBytes(data);
+
+int timeout = 120;
+
+
+
+
+
